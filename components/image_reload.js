bbcca095e18a2040be242d9b0688b62ee96eacec
cirilo007 <cyril.boudou@gmail.com>
2017-06-26 13:24:52 -0400

    new version

--- components/image_reload.js
+++ components/image_reload.js
@@ -7,34 +7,27 @@ export default class ImageReload extends React.Component {
   constructor(props) {
          super(props);
          this.state = {
-           imageurl: "http://" + this.props.ip + "/img/home.jpg",
+           url: "http://" + this.props.ip + "/img/home.jpg",
            size: "dimensions",
 
          };
          this.tick = this.tick.bind(this);
-         this.showsize = this.showsize.bind(this);
          this.componentDidMount = this.componentDidMount.bind(this);
      }
 
 
-  tick() {
-    this.setState({url: "http://" + this.props.ip + "/img/home.jpg?time="+new Date().getTime()});
-  }
-
   componentDidMount() {
     this.interval = setInterval(
-      this.tick,1000);
+      this.tick,2000);
 
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
 
-  showsize(){
+
+  tick(){
 
     var req = new XMLHttpRequest();
-    req.open('GET', this.state.imageurl, false);
+    req.open('GET', "http://" + this.props.ip + "/img/home.jpg?time="+new Date().getTime(), false);
     req.send(null);
     var bytes = req.getResponseHeader("Content-Length").toLowerCase();
 
@@ -42,11 +35,17 @@ export default class ImageReload extends React.Component {
 
     if(bytes > 9000){
       this.setState({color: "bg-success" });
+
+
     } else {
       this.setState({color: "bg-danger" });
     }
 
+    if(bytes == 4096 || bytes == 0){
+        this.setState({color: "bg-success" });
+    }
 
+    this.setState({url: "http://" + this.props.ip + "/img/home.jpg?time="+new Date().getTime()});
 
     //  alert( img.width+' '+ img.height );
 
@@ -58,9 +57,10 @@ export default class ImageReload extends React.Component {
       return (
       <div className={this.state.color}>
         <h3>{this.props.name}</h3>
-          <small>{this.state.bytes} kb</small>
+          <small>{this.state.bytes} kb </small>
           <br />
-          <img src={this.state.url} className="img-responsive" onLoad={this.showsize} />
+
+          <img src={this.state.url} className="img-responsive" />
       </div>
     );
   }
