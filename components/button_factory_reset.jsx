0cff9068972ef58263b72d7cd9171c854aa4300d
cirilo007 <cyril.boudou@gmail.com>
2017-07-11 13:52:29 -0400

    new version

--- components/button_factory_reset.jsx
+++ components/button_factory_reset.jsx
@@ -12,6 +12,14 @@ export default class ButtonReset extends React.Component {
 
   factoryReset(e) {
     alert("boom");
+    exec('sh lirc_fr.sh',
+      function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+          console.log('exec error: ' + error);
+        }
+    });
   }
 
   render() {
